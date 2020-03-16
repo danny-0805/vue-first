50d1503fccbb5acfa380f47e1c0cfd9255695203
Denis <dan.devguru@gmail.com>
2020-03-16 03:50:25 +0300

    Create methods

--- main.js
+++ main.js
@@ -2,6 +2,30 @@ var app = new Vue({
   el: "#app",
   data: {
     product: "Socks",
-    image: "./assets/vmSocks-green.png"
+    image: "./assets/vmSocks-green.png",
+    inStock: true,
+    onSale: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantColor: "green",
+        variantId: 2234,
+        variantImage: "./assets/vmSocks-green.png"
+      },
+      {
+        variantColor: "blue",
+        variantId: 2235,
+        variantImage: "./assets/vmSocks-blue.png"
+      }
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart: function() {
+      this.cart += 1;
+    },
+    updateProduct: function(variantImage) {
+      this.image = variantImage;
+    }
   }
 });
